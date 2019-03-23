53578cbd275b3b64422e36277fb737044c824ab7
Felice Forby <feliceforby@gmail.com>
2019-03-23 01:24:49 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return {
+        friends: [...state.friends, action.friend]
+      }
+    default:
+      return state;
+  }
 }
