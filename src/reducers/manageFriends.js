20ce17b8ead8584f9fa815a10f307b01e1af487d
Felice Forby <feliceforby@gmail.com>
2019-03-23 01:46:14 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -10,6 +10,16 @@ export function manageFriends(state, action) {
       let filteredFriends = friends.filter(function(friend, index, array) {
         return friend.id !== action.id;
       });
+    // Other option:
+    // case "REMOVE_FRIEND":
+    //   const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+    //   return Object.assign({}, state, {
+    //     friends: [
+    //       ...state.friends.slice(0, removalIndex),
+    //       ...state.friends.slice(removalIndex + 1)
+    //     ]
+    //   });
+
       
       return { friends: filteredFriends };
     default:
