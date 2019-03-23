7e5abb82cad54fca85f27705fcf576bc2ceefa25
Felice Forby <feliceforby@gmail.com>
2019-03-23 01:46:48 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return {
+        friends: [...state.friends, action.friend]
+      };
+    case "REMOVE_FRIEND":
+      let friends = [...state.friends];
+      
+      let filteredFriends = friends.filter(function(friend, index, array) {
+        return friend.id !== action.id;
+      });
+      
+      return { friends: filteredFriends };
+    // Other option:
+    // case "REMOVE_FRIEND":
+    //   const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+    //   return Object.assign({}, state, {
+    //     friends: [
+    //       ...state.friends.slice(0, removalIndex),
+    //       ...state.friends.slice(removalIndex + 1)
+    //     ]
+    //   });
+    default:
+      return state;
+  }
 }
